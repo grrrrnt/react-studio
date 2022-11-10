25b53ae394109cfe7d786ce7b5c0d1f416c151e9
grrrrnt <grantlee97@gmail.com>
2022-11-10 16:04:34 -0500

    Complete assignment

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import { useState } from "react";
 import bakeryData from "./assets/bakery-data.json";
+import BakeryItem from "./components/BakeryItem";
 
 /* ####### DO NOT TOUCH -- this makes the image URLs work ####### */
 bakeryData.forEach((item) => {
@@ -9,21 +10,73 @@ bakeryData.forEach((item) => {
 /* ############################################################## */
 
 function App() {
-  // TODO: use useState to create a state variable to hold the state of the cart
-  /* add your cart state code here */
+  const [cart, setCart] = useState([]); // List of items in the cart
 
   return (
     <div className="App">
-      <h1>My Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
-
-      {bakeryData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
-        <p>Bakery Item {index}</p> // replace with BakeryItem component
-      ))}
-
-      <div>
-        <h2>Cart</h2>
-        {/* TODO: render a list of items in the cart */}
+      <h1 className="App-title">Grant's Bakery</h1>
+      <div className="App-content">
+        <div className="bakery-items-columns">
+          {bakeryData.map((item, _) => (
+            <BakeryItem item={item} cart={cart} setCart={setCart} />
+          ))}
+        </div>
+        <div className="cart-column-empty" />
       </div>
+      <div className="cart-column">
+        <h2>Cart</h2>
+        {renderCartItems(cart)}
+        {renderCartTotal(cart)}
+        <button
+          onClick={() => window.open("https://www.youtube.com/watch?v=7T1OHha9agg", '_blank')}
+          className="checkout-button">Check Out</button>
+      </div>
+    </div>
+  );
+}
+
+function renderCartItems(cart) {
+  if (cart.length === 0) {
+    return (
+      <div>
+        <p>Your cart is empty. Start shopping now!</p>
+      </div>
+    );
+  }
+
+  var qty = {}
+  for (var i in cart) {
+    if (cart[i].name in qty) {
+      qty[cart[i].name] += 1;
+    } else {
+      qty[cart[i].name] = 1;
+    }
+  }
+
+  return (
+    <div>
+      {
+        Object.entries(qty).map(([key, value]) => {
+           return (
+            <div className="cart-item">
+              <p>{value} x {key}</p>
+            </div>
+          )
+        })
+      }
+    </div>
+  );
+}
+
+function renderCartTotal(cart) {
+  var total = 0;
+  for (var i in cart) {
+    total += cart[i].price;
+  }
+
+  return (
+    <div className="cart-total">
+      <p>Total: ${total.toFixed(2)}</p>
     </div>
   );
 }
--- src/components/BakeryItem.js
+++ src/components/BakeryItem.js
@@ -1,1 +1,19 @@
-// TODO: create a component that displays a single bakery item
+import "./BakeryItem.css"
+
+export default function BakeryItem({ item, cart, setCart }) {
+  return (
+    <div className="bakery-item">
+      <img src={item.image} alt={item.name} />
+      <div className="bakery-item-content">
+        <div className="bakery-item-name-description">
+          <h3>{item.name}</h3>
+          <p>{item.description}</p>
+        </div>
+        <div className="bakery-item-price-buy">
+          <p>${item.price}</p>
+          <button onClick={() => setCart([...cart, item])}>Add to Cart</button>
+        </div>
+      </div>
+    </div>
+  );
+};
